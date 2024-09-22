2c66bd222b5e39a0658ed912b0b35294f42752c9
zushii <zxgamingh@gmail.com>
2024-09-22 20:59:14 +0200

    Labb 2 Klar

--- labb2.py
+++ labb2.py
@@ -1,16 +1,45 @@
-class Pokemon():
-  def __init__(self, name, lvl):
-    self.name = name
+class Pokemon():                     # Defines a class
+  def __init__(self, name, lvl):     # Constructor to set up values for the Class
+    self.name = name                 # Attributes for the Class
     self.lvl = lvl
     self.hp = 100
   
-  def stats(self):
-    print(f"{self.name} is lvl {self.lvl} and has {self.hp} hp.")
+  def stats(self):                   # Method called stats() in the Class
+    return f"{self.name} is lvl {self.lvl} and has {self.hp} hp." # Returns a string with the pokemons statistics.
   
-class FirePokemon():
+class FirePokemon(Pokemon):
   def __init__(self, name, lvl):
-    super("Pokemon").__init__(name, lvl)
+    super().__init__(name, lvl)       # Calls the parent Classes __init__
   
-  def fireattack(self):
+  def ember(self):
+    dmg = 75
+    return f"{self.name} used Ember and dealt {dmg} amount of damage!"
+  
+class WaterPokemon(Pokemon):
+  def __init__(self, name, lvl):
+    super().__init__(name, lvl)
+  
+  def watergun(self):
+    dmg = 60
+    return f"{self.name} used Water Gun and dealt {dmg} amount of damage!"
 
-    
+class GrassPokemon(Pokemon):
+  def __init__(self, name, lvl):
+    super().__init__(name, lvl)
+  
+  def vinewhip(self):
+    dmg = 50
+    return f"{self.name} used Vine Whip and dealt {dmg} amount of damage!"
+
+bulbasaur = GrassPokemon("Bulbasaur", 5)
+charmander = FirePokemon("Charmander", 5)
+squirtle = WaterPokemon("Squirtle", 5)
+
+print(charmander.stats())             # Prints the value returned in the specified method.
+print(charmander.ember())
+
+print(squirtle.stats())
+print(squirtle.watergun())
+
+print(bulbasaur.stats())
+print(bulbasaur.vinewhip())
